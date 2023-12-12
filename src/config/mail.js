ddba926856a5b38d6c826c00776bb6e372fa9dae
CancianiWill <williamcanciani@gmail.com>
2023-12-12 00:09:49 -0300

    att enviando emails

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+        host: "sandbox.smtp.mailtrap.io",
+        port: 2525,
+        secure: false,
+        auth: {
+          user: "e5e3fd789195ff",
+          pass: "b0051b48cd7f8f"
+        },
+        default: {
+            from: "Sistema <naoresponda@exemplo.com"
+        }
+}
